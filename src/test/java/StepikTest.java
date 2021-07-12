c0244f78957e0a85436bc06594421b4ad4548300
ivan.mikhaylov <mikhaylovivan90@mail.ru>
2021-07-12 20:22:53 +0400

    initial commit

---
+++ src/test/java/StepikTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
+public class StepikTest {
+
+    @Test
+    void test1() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(primeFactors(n));
+    }
+
+    private String primeFactors(final int n) {
+        String result = "";
+        boolean endFlag = false;
+        int i = 2;
+        while (!endFlag) {
+        }
+
+        return null;
+    }
+}
